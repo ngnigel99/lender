c4d1caf5a07f073f4287e38646dbaa30fdacffb5
ngnigel99 <e0543444@u.nus.edu>
2021-08-20 17:51:50 +0800

    add mechanism to track master

--- src/main/java/Loan.java
+++ src/main/java/Loan.java
@@ -3,12 +3,20 @@ public class Loan {
     private int amount;
     private boolean isPaid;
     private double interestRate;
+    private static double maxLoanAmount;
+
+    public static double getMaxLoanAmount() {
+        return maxLoanAmount;
+    }
 
     public Loan(String borrower, int amount, double interestRate) {
         this.borrower = borrower;
         this.amount = amount;
         this.interestRate = interestRate;
         this.isPaid = false;
+        if (this.amount > maxLoanAmount) {
+            maxLoanAmount = this.amount;
+        }
     }
 
     public void setAsPaid() {
